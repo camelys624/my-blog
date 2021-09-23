26c1d3cab9f398294305739aae160fbfb96aac6a
camelys624 <camel_yangz@163.com>
2021-09-23 17:43:49 +0800

    css houdini & resume

---
+++ src/components/business-card/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import businessCardStyle from './index.module.css';
+
+export default (props) => {
+    return (
+        <div className={businessCardStyle.businessContainer}>
+            {/*<div>*/}
+            {/*    <div>*/}
+            {/*        <h1>杨升</h1>*/}
+            {/*        <div>WEB前端开发工程师</div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <div>*/}
+            {/*            <label><i/></label>*/}
+            {/*            <div>15922835412</div>*/}
+            {/*        </div>*/}
+            {/*        <div>*/}
+            {/*            <label><i/></label>*/}
+            {/*            <div>camel_yangz@163.com</div>*/}
+            {/*        </div>*/}
+            {/*        <div>*/}
+            {/*            <label><i/></label>*/}
+            {/*            <div>重庆市高新区</div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <h1>CSS Houdini</h1>
+        </div>
+    )
+}
