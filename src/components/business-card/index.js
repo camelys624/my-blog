514d9c2f263c34bba1733b2ccd7291d8ef684796
camelys624 <camel_yangz@163.com>
2021-09-25 15:31:41 +0800

    resume update

--- src/components/business-card/index.js
+++ src/components/business-card/index.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import businessCardStyle from './index.module.css';
+import React from "react"
+import businessCardStyle from "./index.module.css"
 
 export default (props) => {
-    return (
-        <div className={businessCardStyle.businessContainer}>
-            {/*<div>*/}
-            {/*    <div>*/}
-            {/*        <h1>杨升</h1>*/}
-            {/*        <div>WEB前端开发工程师</div>*/}
-            {/*    </div>*/}
-            {/*    <div>*/}
-            {/*        <div>*/}
-            {/*            <label><i/></label>*/}
-            {/*            <div>15922835412</div>*/}
-            {/*        </div>*/}
-            {/*        <div>*/}
-            {/*            <label><i/></label>*/}
-            {/*            <div>camel_yangz@163.com</div>*/}
-            {/*        </div>*/}
-            {/*        <div>*/}
-            {/*            <label><i/></label>*/}
-            {/*            <div>重庆市高新区</div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            <h1>CSS Houdini</h1>
-        </div>
-    )
+  return (
+    <div className={businessCardStyle.businessContainer}>
+      {/*<div>*/}
+      {/*    <div>*/}
+      {/*        <h1>杨升</h1>*/}
+      {/*        <div>WEB前端开发工程师</div>*/}
+      {/*    </div>*/}
+      {/*    <div>*/}
+      {/*        <div>*/}
+      {/*            <label><i/></label>*/}
+      {/*            <div>15922835412</div>*/}
+      {/*        </div>*/}
+      {/*        <div>*/}
+      {/*            <label><i/></label>*/}
+      {/*            <div>camel_yangz@163.com</div>*/}
+      {/*        </div>*/}
+      {/*        <div>*/}
+      {/*            <label><i/></label>*/}
+      {/*            <div>重庆市高新区</div>*/}
+      {/*        </div>*/}
+      {/*    </div>*/}
+      {/*</div>*/}
+      <h1>CSS Houdini</h1>
+    </div>
+
+  )
 }
